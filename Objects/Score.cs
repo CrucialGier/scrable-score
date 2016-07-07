ecac7029901f6a6bb458d4215727da1e4764fd87
Shradha Pulla & Stewart Cole <pullashradha@gmail.com>
2016-07-07 09:56:06 -0700

    Trying to program first spec

--- Objects/Score.cs
+++ Objects/Score.cs
@@ -6,6 +6,40 @@ namespace Scrabble.Objects
 {
   public class Score
   {
-    
+    private char _letterInput;
+    // private string[] _letterScore1 = ["a","e","i","o","u","l","n","r","s","t"];
+    private string _letterScore1 = "aeioulnrst";
+    private int _letterScore = 0;
+
+    public char GetLetterInput()
+    {
+      return _letterInput;
+    }
+    public void SetLetterInput(char newLetterInput)
+    {
+      _letterInput = newLetterInput;
+    }
+
+    public int GetScore()
+    {
+      foreach (char letter in _letterScore1)
+      {
+        if (letter == _letterInput)
+        {
+          return _letterScore += 1;
+        }
+      }
+      return _letterScore;
+    }
   }
 }
+
+
+
+// for (int i = 0; i < _letterScore1.Length; i++)
+// {
+//   if (_letterScore1.charAt(i) == _letterInput)
+//   {
+//     return _letterScore += 1;
+//   }
+// }
--- Tests/ScoreTest.cs
+++ Tests/ScoreTest.cs
@@ -1,5 +1,17 @@
 using Xunit;
-namespace Scrabble
-{
 
+namespace Scrabble.Objects
+{
+  public class ScoreTest
+  {
+    [Fact]
+    public void GetScore_SingleLetterScore_1()
+    {
+      Score newScoreTest = new Score ();
+      Assert.Equal(1, newScoreTest.GetScore());
+      // Score.SetLetterInput('l');
+      // Score.GetScore();
+      // Assert.Equal(1, _letterScore);
+    }
+  }
 }
