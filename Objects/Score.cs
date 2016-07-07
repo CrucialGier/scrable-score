7bbe846980ae8dcefea31df74f44a8ca95611a88
Shradha Pulla & Stewart Cole <pullashradha@gmail.com>
2016-07-07 11:54:43 -0700

    Refactor GetScore method in Score.cs

--- Objects/Score.cs
+++ Objects/Score.cs
@@ -7,29 +7,67 @@ namespace Scrabble.Objects
 {
   public class Score
   {
-    private char _letterInput;
-    private string _letterScore1 = "aeioulnrst";
+    private char _wordInput;
+    private static string _letterScore1 = "aeioulnrst";
+    private static string _letterScore2 = "dg";
+    private static string _letterScore3 = "bcmp";
+    private static string _letterScore4 = "fhvwy";
+    private static string _letterScore5 = "k";
+    private static string _letterScore8 = "jx";
+    private static string _letterScore10 = "qz";
+    private static string[] _allLettersArray = {_letterScore1, _letterScore2, _letterScore3, _letterScore4, _letterScore5, _letterScore8, _letterScore10};
     private int _letterScore = 0;
 
     public char GetLetterInput()
     {
-      return _letterInput;
+      return _wordInput;
     }
     public void SetLetterInput(char newLetterInput)
     {
-      _letterInput = newLetterInput;
+      _wordInput = newLetterInput;
     }
 
     public int GetScore()
     {
-      foreach (char letter in _letterScore1)
+      foreach (string letterGroup in _allLettersArray)
       {
-        if (letter == _letterInput)
+        foreach (char letter in letterGroup)
         {
-          return _letterScore += 1;
+          if (letter == _wordInput)
+          {
+            if (letterGroup == _letterScore1)
+            {
+              return _letterScore += 1;
+            }
+            else if (letterGroup == _letterScore2)
+            {
+              return _letterScore += 2;
+            }
+            else if (letterGroup == _letterScore3)
+            {
+              return _letterScore += 3;
+            }
+            else if (letterGroup == _letterScore4)
+            {
+              return _letterScore += 4;
+            }
+            else if (letterGroup == _letterScore5)
+            {
+              return _letterScore += 5;
+            }
+            else if (letterGroup == _letterScore8)
+            {
+              return _letterScore += 8;
+            }
+            else
+            {
+              return _letterScore += 10;
+            }
+          }
         }
       }
       return _letterScore;
     }
+    
   }
 }
