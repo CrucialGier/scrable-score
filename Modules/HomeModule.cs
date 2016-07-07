569dc3b76d5538ec7b1ca210afc9f497a1b51625
Shradha Pulla & Stewart Cole <pullashradha@gmail.com>
2016-07-07 08:22:58 -0700

    Create basic files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Scrabble.Objects;
+using System.Collections.Generic;
+
+namespace Scrabble
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
---
+++ Objects/Score.cs
@@ -0,0 +1,11 @@
+using Nancy;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Scrabble.Objects
+{
+  public class Score
+  {
+    
+  }
+}
