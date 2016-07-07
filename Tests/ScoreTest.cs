1d4a50ee54b94ab1996c8a0b91b9b67cbbafc742
Shradha Pulla & Stewart Cole <pullashradha@gmail.com>
2016-07-07 13:39:00 -0700

    Add test method to ScoreTest.cs

--- Tests/ScoreTest.cs
+++ Tests/ScoreTest.cs
@@ -5,11 +5,18 @@ namespace Scrabble.Objects
   public class ScoreTest
   {
     [Fact]
-    public void GetScore_SingleLetterScore_1()
+    public void GetScore_SingleLetterScore_true()
     {
       Score newScoreTest = new Score ();
       //Make all variables & methods static to avoid creating a new object
       newScoreTest.SetLetterInput("l");
+      Assert.Equal(1, newScoreTest.GetScore());
+    }
+    [Fact]
+    public void GetScore_WholeWordScore_false()
+    {
+      Score newScoreTest = new Score ();
+      newScoreTest.SetLetterInput("adbfkjq");
       Assert.Equal(1, newScoreTest.GetScore());
     }
   }
